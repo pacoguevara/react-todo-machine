ea4ff57acbc65c52f23bbbabf951870a76404586
Paco Guevara <pacoguevaraa@gmail.com>
2022-01-08 23:10:32 -0800

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
+import React from "react";
+
+const todos = [
+  { text: 'This is a todo', completed: false},
+  { text: 'This is a todo 1', completed: false},
+  { text: 'This is a todo 2', completed: false}
+]
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	return (
+		<React.Fragment>
+			<TodoCounter />
+			<TodoSearch />
+			<TodoList>
+				{
+					todos.map(todo => (
+						<TodoItem/>
+					))
+				}
+			</TodoList>
+			<CreateTodoButton />
+		</React.Fragment>
   );
 }
 
